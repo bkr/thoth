d6b5d7cb2d8ac6614c44cecc81cc5d06be4906f9
Philippe Huibonhoa <phuibonhoa@gmail.com>
2014-09-27 01:44:19 -0700

    Updated default logger to always flush.

--- lib/thoth/rails/railtie.rb
+++ lib/thoth/rails/railtie.rb
@@ -4,7 +4,11 @@ require 'thoth/rails/model'
 module Thoth
   class Railtie < ::Rails::Railtie
     initializer "thoth.configure_rails_initialization" do
-      Thoth.logger ||= Logger.new(Output::Json.new(File.open(::Rails.root.join(*%W[log events_#{ENV['RAILS_ENV']}.log]), 'a')))
+      Thoth.logger ||= (
+        file = File.open(::Rails.root.join(*%W[log events_#{ENV['RAILS_ENV']}.log]), 'a')
+        file.sync = true
+        Logger.new(Output::Json.new(file))
+      )
     end
   end
 end
